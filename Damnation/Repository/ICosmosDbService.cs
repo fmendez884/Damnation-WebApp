4e4f26be7bb564369d6d6355e666618679ecd0b4
Francis <fmendez884@gmail.com>
2020-11-15 07:34:31 -0500

    Adds API controller, cosmosDb services

---
+++ Damnation/Repository/ICosmosDbService.cs
@@ -0,0 +1,22 @@
+﻿namespace Damnation
+{
+    using Damnation.Models;
+    using Newtonsoft.Json.Linq;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+
+        //Task<JArray> GetItemsAsync(string collectionName, JArray items);
+        //Task<JObject> GetItemAsync(string collectionName, JObject item);
+        //Task AddItemAsync(string collectionName, JObject item);
+        //Task UpdateItemAsync(string id, string collectionName, JObject item);
+        //Task DeleteItemAsync(string id, string collectionName, JObject item);
+    }
+}
