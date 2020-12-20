14f14e0d4ae7e19dabcd74877492470abe77ab7c
Francis <fmendez884@gmail.com>
2020-12-20 10:59:10 -0500

    fix

--- Damnation/ClientApp/src/components/WebPlayer/GameContainer.tsx
+++ Damnation/ClientApp/src/components/WebPlayer/GameContainer.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDom from "react-dom"
+import * as React from "react";
+import * as ReactDom from "react-dom"
 import { connect } from "react-redux";
 
 import DamnationWebPlayer from "./DamnationWebPlayer";
